df1bfd954e6cda49233f03029ead140fd6817ef9
Thanuji <thanujiisurikalive@gmail.com>
2024-01-18 16:28:02 +0000

    Test implementation

---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,100 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class TicketServiceImplTest {
+  @Mock
+    SeatReservationServiceImpl seatReservationService;
+
+  @Mock
+    TicketPaymentServiceImpl ticketPaymentService;
+
+  @InjectMocks
+  TicketServiceImpl ticketServiceImpl;
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+  @Before
+  public void setUp(){
+       ticketServiceImpl = new TicketServiceImpl();
+      initMocks(this);
+    }
+  @Test
+    public void purchaseTicketsOK1(){
+    ticketServiceImpl.purchaseTickets(1L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2),new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2));
+  }
+
+  @Test
+  public void purchaseTicketsOK2(){
+    ticketServiceImpl.purchaseTickets(1L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2),new TicketTypeRequest(null, 2));
+  }
+
+  @Test
+  public void testPurchaseTickets1() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(null, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, -1));
+  }
+
+  @Test
+  public void testPurchaseTickets2() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(null, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+  }
+
+  @Test
+  public void testPurchaseTickets3() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(-1L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+  }
+
+  @Test
+  public void testPurchaseTickets4() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(1L, new TicketTypeRequest(null, 1));
+  }
+
+
+  @Test
+  public void testPurchaseTickets5() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(1L, new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1));
+  }
+
+
+  @Test
+  public void testPurchaseTickets6() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(1L, new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1));
+  }
+
+  @Test
+  public void testPurchaseTickets7() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(1L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 0));
+  }
+
+
+  @Test
+  public void testPurchaseTickets8() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(1L, (TicketTypeRequest) null);
+  }
+
+
+  @Test
+  public void testPurchaseTickets9() throws InvalidPurchaseException {
+    thrown.expect(InvalidPurchaseException.class);
+    ticketServiceImpl.purchaseTickets(1L);
+  }
+
+}
